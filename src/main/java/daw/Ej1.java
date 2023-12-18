eb57ee3604ff7e691c0efc1c6a69286c514a318a
jaceara2006 <jaceara2006@g.educaand.es>
2023-12-18 13:01:48 +0100

    Ej2 Terminado

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -2,6 +2,7 @@ package daw;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -13,7 +14,7 @@ public class Ej1 {
         // Inicializo TAMAÑO.
         final int TAMANO = 15;
 
-        // Crear un array con el tamaño adecuado.
+        // Creo un array con el tamaño adecuado.
         double[] miarray = new double[TAMANO];
 
         // Relleno todas las posiciones con 7.5
@@ -21,15 +22,17 @@ public class Ej1 {
             miarray[i] = 7.5;
         }
 
-        // Crear un array copia.
+        // Creo un array copia de miarray.
         double[] miarraycopia = miarray.clone();
 
+        // Imprimo ambos arrays por consola usando toString().
         System.out.println("----------------");
         System.out.println("Miarray: " + Arrays.toString(miarray) + ".");
         System.out.println("----------------");
         System.out.println("MiarrayCopia: " + Arrays.toString(miarraycopia) + ".");
         System.out.println("----------------");
 
+        // Comparo ambos arrays para ver si son iguales.
         if (Arrays.equals(miarray, miarraycopia)) {
             System.out.println("Ambos arrays son iguales.");
         } else {
@@ -43,16 +46,53 @@ public class Ej1 {
         int posicion = random.nextInt(TAMANO);
         miarray[posicion] = 6.3;
 
+        // Imprimo ambos arrays por consola usando toString().
         System.out.println("----------------");
         System.out.println("Miarray: " + Arrays.toString(miarray) + ".");
         System.out.println("----------------");
         System.out.println("MiarrayCopia: " + Arrays.toString(miarraycopia) + ".");
         System.out.println("----------------");
 
+        // Comparo ambos arrays para ver si son iguales.
         if (Arrays.equals(miarray, miarraycopia)) {
             System.out.println("Ambos arrays son iguales.");
         } else {
             System.out.println("Ambos arrays son diferentes.");
+        }
+
+        System.out.println("----------------");
+        
+        // Genero un array int con valores aleatorios.
+        int[] arrayAleatorio = new int[TAMANO];
+        llenarArrayAleatorio(arrayAleatorio);
+
+        // Ordeno el array.
+        Arrays.sort(arrayAleatorio);
+        System.out.println("----------------");
+        System.out.println("Array ordenado: " + Arrays.toString(arrayAleatorio) + ".");
+        System.out.println("----------------");
+
+        // Preguntar al usuario.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número a buscar: ");
+        int numeroBuscar = scanner.nextInt();
+
+        int ubicacion = Arrays.binarySearch(arrayAleatorio, numeroBuscar);
+
+        if (ubicacion < 0) {
+            System.out.println("El elemento no se encuentra en el array.");
+        } else {
+            System.out.println("El elemento se encuentra en la posición " + ubicacion + " del array.");
+        }
+
+    }
+
+    public static void llenarArrayAleatorio(int[] array) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            int aleatorio = random.nextInt(10, 101);
+            array[i] = aleatorio;
         }
     }
 }
