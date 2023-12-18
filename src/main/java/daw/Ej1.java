d712a2da969e3e4e0eb2e055c12a4edc65cbab76
jaceara2006 <jaceara2006@g.educaand.es>
2023-12-18 11:00:31 +0100

    Ej1 Terminado

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -1,5 +1,8 @@
 package daw;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author acebedo
@@ -7,6 +10,49 @@ package daw;
 public class Ej1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Inicializo TAMAÑO.
+        final int TAMANO = 15;
+
+        // Crear un array con el tamaño adecuado.
+        double[] miarray = new double[TAMANO];
+
+        // Relleno todas las posiciones con 7.5
+        for (int i = 0; i < miarray.length; i++) {
+            miarray[i] = 7.5;
+        }
+
+        // Crear un array copia.
+        double[] miarraycopia = miarray.clone();
+
+        System.out.println("----------------");
+        System.out.println("Miarray: " + Arrays.toString(miarray) + ".");
+        System.out.println("----------------");
+        System.out.println("MiarrayCopia: " + Arrays.toString(miarraycopia) + ".");
+        System.out.println("----------------");
+
+        if (Arrays.equals(miarray, miarraycopia)) {
+            System.out.println("Ambos arrays son iguales.");
+        } else {
+            System.out.println("Ambos arrays son diferentes.");
+        }
+
+        System.out.println("----------------");
+
+        // Cambiar un valor en una posición aleatoria de miarray.
+        Random random = new Random();
+        int posicion = random.nextInt(TAMANO);
+        miarray[posicion] = 6.3;
+
+        System.out.println("----------------");
+        System.out.println("Miarray: " + Arrays.toString(miarray) + ".");
+        System.out.println("----------------");
+        System.out.println("MiarrayCopia: " + Arrays.toString(miarraycopia) + ".");
+        System.out.println("----------------");
+
+        if (Arrays.equals(miarray, miarraycopia)) {
+            System.out.println("Ambos arrays son iguales.");
+        } else {
+            System.out.println("Ambos arrays son diferentes.");
+        }
     }
 }
